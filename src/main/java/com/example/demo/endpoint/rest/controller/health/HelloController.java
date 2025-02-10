f5fda7765703b59d33471297f411b105288beb1a
Harizo ANDRIANAIVO <124563616+Eclisher@users.noreply.github.com>
2025-02-10 15:19:42 +0300

    feat:add the hellocontroller

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import com.example.demo.PojaGenerated;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hell-std22007";
+  }
+}
